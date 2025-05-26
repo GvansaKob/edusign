f2b7c2c1847050d4fd59c8f869c5bff3262d570b
GvansaKob <gvansa.kobesashvili@my-digital-school.org>
2025-05-26 21:55:25 +0200

    Add QR code régénère (5 secs)

--- front-web/src/components/QRCodeDisplay.tsx
+++ front-web/src/components/QRCodeDisplay.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-import { QRCodeCanvas } from "qrcode.react";
+import React, { useEffect, useState } from "react";
+import QRCode, { QRCodeCanvas } from "qrcode.react";
 
-interface Props {
+interface QRCodeDisplayProps {
   sessionId: string;
 }
 
-export default function QRCodeDisplay({ sessionId }: Props) {
-  const scanUrl = `https://ton-domaine.com/scan/${sessionId}`;
+const QRCodeDisplay: React.FC<QRCodeDisplayProps> = ({ sessionId }) => {
+  const [qrValue, setQrValue] = useState(`${sessionId}-${Date.now()}`);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setQrValue(`${sessionId}-${Date.now()}`);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [sessionId]);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h2>Scannez ce QR Code pour signer</h2>
-      <QRCodeCanvas value={scanUrl} size={256} />
+    <div className="flex justify-center my-6">
+      <QRCodeCanvas value={qrValue} size={200} />
     </div>
   );
-}
+};
+
+export default QRCodeDisplay;
